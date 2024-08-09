5583f11e8c7f899ce0a6ff640607d89d86700d13
RuslanAndr1107 <androshulikruslan@gmail.com>
2024-08-09 18:59:28 +0300

    2
    2

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,6 +1,6 @@
 export default function getPictures(name) {
   const BASE_URL = "https://pixabay.com/api/";
-  const KEY = "45109614-503c0802e6fcda244df0afcb5";
+  const KEY = "45357458-06e8a1a2f264c6c9b3b3f40d6";
 
   if (name.includes(" ")) {
     name.replace(/\s+/g, "+");
